f73317fbccdae70e9ed25a4c5b2b113ce8e4d517
RenBolen <rdbolen42@gmail.com>
2026-09-20 19:56:18 +0000

    Finished HW 3

--- 100_HW/HW3_Divisible/base_code/starter.java
+++ 100_HW/HW3_Divisible/base_code/starter.java
@@ -1,6 +1,6 @@
 /*
- *	Author:
- *  Date:
+ *	Author: Ren Bolen
+ *  Date:September 20, 2026
  * 	Collaborator: 
 */
 
@@ -8,6 +8,38 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Choose an integer: ");
+		int num = sc.nextInt();
+		System.out.print("Choose another integer: ");
+		int num2 = sc.nextInt();
 		
+		String numStatus = "odd";
+		boolean evenOdd = num % 2 == 0;
+		if (evenOdd == true) {
+			numStatus = "even";
+		}
+		String divisible = "is not ";
+		if (num % 3 + num % 4 + num % 5 == 0) {
+			divisible = "is ";
+		}
+		String numStatus2 = "odd";
+		boolean evenOdd2 = num2 % 2 == 0;
+		if (evenOdd2 == true) {
+			numStatus2 = "even";
+		}
+		String divisible2 = "is not ";
+		if (num2 % 3 + num2 % 4 + num2 % 5 == 0) {
+			divisible2 = "is ";
+		}
+
+		System.out.println();
+		System.out.println("The number " + num + " is " + numStatus + ".");
+		System.out.println(num + " " + divisible + "divisible by 3, 4, and 5.");
+		System.out.println();
+		System.out.println("The number " + num2 + " is " + numStatus2 + ".");
+		System.out.println(num2 + " " + divisible2 + "divisible by 3, 4, and 5.");
+
+
 	}
 }
--- 102_Exercises/10_RandomRange/base_code/starter.java
+++ 102_Exercises/10_RandomRange/base_code/starter.java
@@ -8,8 +8,25 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 2 numbers to create a range for your random number.");
+		System.out.print("Please enter an integer: ");
+		int number1 = sc.nextInt();
+		System.out.print("Please enter another integer (bigger than the first): ");
+		int number2 = sc.nextInt();
+		System.out.println();
+		System.out.println("Your range is " + number1 + " to " + number2);
+		System.out.println("Here are five random numbers within that range.");
+		int random = (int)(Math.random() * (number2 - number1) + number1);
+		System.out.print(random + ", ");
+		random = (int)(Math.random() * (number2 - number1) + number1);
+		System.out.print(random + ", ");
+		random = (int)(Math.random() * (number2 - number1) + number1);
+		System.out.print(random + ", ");
+		random = (int)(Math.random() * (number2 - number1) + number1);
+		System.out.print(random + ", ");
+		random = (int)(Math.random() * (number2 - number1) + number1);
+		System.out.println(random);
+
 	}
 }
