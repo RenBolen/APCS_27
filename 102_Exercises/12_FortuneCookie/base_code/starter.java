04104dd1e5e47c590a3d94f56ced28f0a6b820e3
RenBolen <rdbolen42@gmail.com>
2026-09-22 19:30:58 +0000

    Finished lab 15

--- 102_Exercises/12_FortuneCookie/base_code/starter.java
+++ 102_Exercises/12_FortuneCookie/base_code/starter.java
@@ -8,6 +8,36 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		
+		Int cookie = (int) (Math.random() * 10 + 1)
+		if (cookie == 1) {
+			System.out.println("Ren will become a genius in the near future!");
+		}
+		if (cookie == 2) {
+			System.out.println("A guy named Ren will become very rich within a few years!");
+		}
+		if (cookie == 3) {
+			System.out.println("Ren will invent the cure to cancer!");
+		}
+		if (cookie == 4) {
+			System.out.println("Branden will admit that Ren is the smartest in the class!");
+		}
+		if (cookie == 5) {
+			System.out.println("Justin will support the fact that Ren is a genius");
+		}
+		if (cookie == 6) {
+			System.out.println("Ren will become the greatest of all GOATs");
+		}
+		if (cookie == 7) {
+			System.out.println("");
+		}
+		if (cookie == 8) {
+			System.out.println("");
+		}
+		if (cookie == 9) {
+			System.out.println("");
+		}
+		if (cookie == 10) {
+			System.out.println("");
+		}
 	}
 }
